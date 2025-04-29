cfd1763ba081d99e0540a4b18750a8d11fc06f6a
belja198 <vlada.beljanski1@gmail.com>
2025-04-29 20:04:28 +0200

    Finale polished
    -added two timers
    -fade to black

--- scenes/levels/level_finale.gd
+++ scenes/levels/level_finale.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 
 @onready var camera: Camera2D = $Camera2D;
+@onready var end_timer: Timer = $EndTimer;
+@onready var end_pause_timer: Timer = $EndPauseTimer;
+var should_darken: bool = false;
+var darken_speed: float = 2.0;
+var darken_degre: float = 0.0;
+
 var should_follow_player: bool = false;
 var end_game: bool = false;
 var camera_speed: float = 30.0;
@@ -32,13 +38,32 @@ func _process(delta: float) -> void:
 		if (index >= 0 && index <= 7):
 				GlobalScript.player_ref.float_sprite.modulate = colors[index];
 
-	if (GlobalScript.player_ref.position.y <= -256.0):
+	if (GlobalScript.player_ref.position.y <= -256.0) && !end_game:
 		end_game = true;
 		GlobalScript.player_ref.visible = false;
 		GlobalScript.player_ref.set_physics_process(false);
+		# start end pause timer
+		end_pause_timer.start();
+		print("first timer started")
 
 	
 
 	if (should_follow_player == true):
 		var target_y: float = GlobalScript.player_ref.position.y
 		camera.position.y = lerp(camera.position.y, target_y, lerp_speed * delta)
+
+	if (should_darken == true):
+		modulate = lerp(modulate, Color(0,0,0), lerp_speed * delta);
+
+
+
+func _on_end_pause_timer_timeout() -> void:
+	end_timer.start();
+	should_darken = true;
+	print("first timer ended")
+
+
+func _on_end_timer_timeout() -> void:
+	print("second timer ended")
+	modulate = Color(0,0,0);
+	get_tree().quit();
